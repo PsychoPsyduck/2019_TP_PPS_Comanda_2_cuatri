b45af32b4a0a906e70808819966e463219a77bb2
gastonpesoa <gastonpesoa@gmail.com>
2019-11-10 11:47:43 -0300

    test 2

---
+++ src/app/clases/mesa.ts
@@ -0,0 +1,16 @@
+export class Mesa {
+    public key: string; // Key de firebase
+    public id: any;
+    public comensales: string;
+    public tipo: string;
+    public foto: any;
+    public estado: string;
+
+    constructor(id?: any, comensales?: string, tipo?: string, foto?: any, estado?: string) {
+        this.id = id;
+        this.comensales = comensales;
+        this.tipo = tipo;
+        this.foto = foto;
+        this.estado = estado;
+    }
+}
---
+++ src/app/clases/pedido.ts
@@ -0,0 +1,16 @@
+import { ProductoPedido } from "./producto-pedido";
+import { Mesa } from './mesa';
+
+export class Pedido {
+  public key: string;
+  public mesa: Mesa;
+  public estado: string;
+  public estadoCocinero: string;
+  public estadoBartender: string;
+  public productoPedido: Array<ProductoPedido>;
+  public isDelivery: boolean;
+  public responsableCocinero: string;
+  public responsableBartender: string;
+  public tiempoDeEsperaCocinero: number;
+  public tiempoDeEsperaBartender: number;
+}
